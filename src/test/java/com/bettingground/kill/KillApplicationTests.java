65b53167c5e71680f5635f5b064f766bf8695391
Panjun-Kwon <pndlm1979@naver.com>
2024-10-01 22:59:38 +0900

    프로젝트 생성

---
+++ src/test/java/com/bettingground/kill/KillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bettingground.kill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
